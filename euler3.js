a8a2356d6003fe0de217a69df256ebfd1bc31642
Zach Webb <zachary.t.webb@gmail.com>
2016-04-08 14:41:04 -0500

    Update

--- euler3.js
+++ euler3.js
@@ -5,7 +5,7 @@ What is the largest prime factor of the number 600851475143 ?
 Just solve for numbers smaller than 1,000 and don't worry about making it efficient. You won't need to do any crazy math -- think iteration and make the computer do the work. Consider it extra credit to make your solution solve for larger numbers in a way that isn't too slow.*/
 
 var primeArray= [];
-var theNumber = 13195; //not using 600851475143 because that's a big-ass number
+var theNumber = 600851475143; 
 
 for (var i = 0; i <= theNumber; i++){
 	if (theNumber % i === 0) {
@@ -22,8 +22,13 @@ for (var i = 0; i <= theNumber; i++){
 		if (prime) {
 			primeArray.push(i);
 			console.log(i + " is definitely a prime factor, yo.")
+			if (i > 1000) {
+				console.log(i + " is over 1000, so we want to stop");
+				break;
+			};
 		};
 	};
 };
 
-console.log(primeArray[primeArray.length - 1] + " is the highest prime factor for " + theNumber);
+console.log(primeArray);
+console.log(primeArray[primeArray.length - 2] + " is the highest prime factor less than 1000 for " + theNumber);
